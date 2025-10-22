422275406e402502dfbfd6999ff459168c1bcf22
Ayushi singh <ayushiisingh08@gmail.com>
2025-10-22 11:04:48 +0530

    project complete

--- client/src/components/MenuItems.jsx
+++ client/src/components/MenuItems.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { menuItemsData } from '../assets/assets';
 import { NavLink } from 'react-router-dom';
 
+
 const MenuItems = ({setSidebarOpen}) => {
   return (
     <div className='px-6 text-gray-600 space-y-1 font-medium'> 
